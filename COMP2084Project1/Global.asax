45c619ca579118244294fc8910ca0b89777fe291
kenalexander <kenoliveralexander@gmail.com>
2018-10-07 15:35:39 -0400

    init commit

---
+++ COMP2084Project1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2084Project1.MvcApplication" Language="C#" %>
---
+++ COMP2084Project1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2084Project1.Startup))]
+namespace COMP2084Project1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
