7f3e928708f7f16bdefb9fa1f730d4a98a9cdf13
kenalexander <kenoliveralexander@gmail.com>
2018-11-25 19:00:00 -0500

    instantiated artTableControllerTest method

---
+++ COMP2084Project1.Tests/Controllers/artTableControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace COMP2084Project1.Tests.Controllers
+{
+    [TestClass]
+    public class artTableControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
